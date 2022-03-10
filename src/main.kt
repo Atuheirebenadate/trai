97b9094c8a0305070e65314a78d85ddc7925c2ff
Your Name <you@example.com>
2022-03-10 00:53:41 -0500

    first commit

---
+++ src/main.kt
@@ -0,0 +1,27 @@
+fun main(){
+mod
+
+}
+fun mod(number:String){
+    var name="CodeHive"
+    var a=(4)
+    var b=(5)
+    var c=(6)
+    var d=(7)
+    println(a)
+    println(b)
+    println(c)
+    println(d)
+}
+fun modulus(x:Int,V:Int){
+
+    var x=18
+    var v=4
+    var module=(x%v)
+    println(x % v)
+
+}
+
+
+
+
